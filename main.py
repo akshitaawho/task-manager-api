eb603b8de245575af4be3975aaa9d6e1d306b69f
Akshitaa Sahoo <it.akshitaa@herofincorp.biz>
2026-06-17 15:26:20 +0530

    Add task creation endpoint

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+tasks = []
+
+
+class Task(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
 
 @app.get("/")
 def home():
     return {"message": "Task Manager API is running"}
+
+
+@app.post("/tasks")
+def create_task(task: Task):
+    task_dict = task.model_dump()
+    task_dict["id"] = len(tasks) + 1
+
+    tasks.append(task_dict)
+
+    return task_dict
